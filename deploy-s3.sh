ccaebd1db0a87c557a548573c96489aaa3b06827
tetsushi-kaneko <tetsushi-kaneko@bm-sms.co.jp>
2020-09-02 19:53:03 +0900

    デプロイ用ファイル修正

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-aws s3 rm s3://sample-hosting-kiyokiyo/ --recursive
-aws s3 cp dist s3://sample-hosting-kiyokiyo/ --recursive
+aws s3 rm s3://sample-hosting-kaneko-redstamp/ --recursive
+aws s3 cp dist s3://sample-kaneko-redstamp/ --recursive
